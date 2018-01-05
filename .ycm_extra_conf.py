70a7707794f2f447ec712ae383efdb9a065cdc3a
Zach Marcantel <zmarcantel@gmail.com>
2018-01-05 23:09:53 +0000

    auto create and refresh commands db

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -7,29 +7,46 @@ import subprocess
 
 #import ycm_core
 
-def FlagsForFile( filename ):
-    flags = []
-    proj_root = os.path.abspath(os.path.dirname(__file__))
-    proj_name = os.path.basename(proj_root)
+def make_ycm_db(proj_root):
+    subprocess.check_call([
+        os.path.join(proj_root, "tools", "compiledb.sh")
+    ])
+
+def get_db_flags(proj_root, rel_fname):
     ycm_db = os.path.join(proj_root, ".ycm_db")
-    rel_fname = os.path.relpath(filename, proj_root)
+
+    # if no db file, make one
+    if not os.path.isfile(ycm_db):
+        make_ycm_db(proj_root)
+
     with open(ycm_db, 'r') as db:
         flist = json.load(db)
-        if rel_fname in flist:
-            flags = flist[rel_fname]
+        if rel_fname in flist: # existing file
+            return flist[rel_fname]
+        else: # newly analyzed file
+            make_ycm_db(proj_root)
+            return get_db_flags(proj_root, rel_fname) # recurse and return from new db
 
-    for (i, f) in enumerate(flags):
-        # abspath all bazel build directories
-        bazel_prefix = "bazel-"
-        if f[0:len(bazel_prefix)] == bazel_prefix:
-            flags[i] = os.path.join(proj_root, f)
-            continue
+def FlagsForFile( filename ):
+    proj_root = os.path.abspath(os.path.dirname(__file__))
+    proj_name = os.path.basename(proj_root)
+    rel_fname = os.path.relpath(filename, proj_root)
 
-        # abspath the bazel external includes
-        external_prefix = "external/"
-        if f[0:len(external_prefix)] == external_prefix:
-            flags[i] = os.path.join(proj_root, os.path.join("bazel-{}".format(proj_name), f))
-            continue
+    flags = get_db_flags(proj_root, rel_fname)
+    if flags is not None:
+        for (i, f) in enumerate(flags):
+            # abspath all bazel build directories
+            if f.startswith("bazel-"):
+                flags[i] = os.path.join(proj_root, f)
+                continue
+
+            # abspath the bazel external includes
+            if f.startswith("external/"):
+                flags[i] = os.path.join(proj_root, os.path.join("bazel-{}".format(proj_name), f))
+                continue
+
+    else:
+        flags = [] # empty.... TODO: error here instead?
 
 
     return {
